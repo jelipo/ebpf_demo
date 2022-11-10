dbe297439506182e04505e73ae8d9428dde11c84
jelipo <me@jelipo.com>
2022-11-10 18:39:15 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+// github.com/bigwhite/experiments/ebpf-examples/helloworld-go/main.go
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go hello c/hello_kern.c
+
+func main() {
+	objects := helloObjects{}
+	err := loadHelloObjects(objects, nil)
+	if err != nil {
+		return
+	}
+
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	// Allow the current process to lock memory for eBPF resources.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %s", err)
+	}
+	defer objs.Close()
+
+	//SEC("tracepoint/syscalls/sys_enter_execve")
+	// attach to xxx
+	kp, err := link.Tracepoint("syscalls", "sys_enter_execve", objs.BpfProg, nil)
+	if err != nil {
+		log.Fatalf("opening tracepoint: %s", err)
+	}
+	defer kp.Close()
+
+	log.Printf("Successfully started! Please run \"sudo cat /sys/kernel/debug/tracing/trace_pipe\" to see output of the BPF programs\n")
+
+	// Wait for a signal and close the perf reader,
+	// which will interrupt rd.Read() and make the program exit.
+	<-stopper
+	log.Println("Received signal, exiting program..")
+}

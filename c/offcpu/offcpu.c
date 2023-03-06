173bf86a402b4c2446853119e8e59bda490f4348
jelipo <me@jelipo.com>
2023-03-07 00:18:14 +0800

    update

---
+++ c/offcpu/offcpu.c
@@ -0,0 +1,31 @@
+#include "../include/vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct event {
+    u32 pid;
+    u32 sig;
+};
+
+struct bpf_map_def SEC("maps") events = {
+        .type = BPF_MAP_TYPE_RINGBUF,
+        .max_entries = 1 << 24,
+};
+
+// Force emitting struct event into the ELF.
+const struct event *unused __attribute__((unused));
+
+SEC("tracepoint/syscalls/sys_enter_kill")
+int ringbuffer_execve(struct trace_event_raw_sys_enter *ctx) {
+    int pid = bpf_get_current_pid_tgid() >> 32;
+    int tpid = ctx->args[0];
+    int sig = ctx->args[1];
+    struct event e = {
+            .pid=tpid,
+            .sig=sig,
+    };
+    bpf_ringbuf_output(&events, &e, sizeof(e), 0);
+    return 0;
+}
